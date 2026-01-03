0ce2d3da7b5a4ecf29d1007e318f7eb57705658b
LennyBo <lenny.boegli@moosvolk.ch>
2026-01-03 14:12:46 +0100

    First version of frontend graphics

--- solnax-frontend/src/app/components/house-graphic/house-graphic.ts
+++ solnax-frontend/src/app/components/house-graphic/house-graphic.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {PowerArrow} from "../power-arrow/power-arrow";
+import {ArrowPoint} from "../../model/arrow-point";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-house-graphic',
-  imports: [],
+  imports: [
+    PowerArrow,
+    NgForOf
+  ],
   templateUrl: './house-graphic.html',
   styleUrl: './house-graphic.scss',
   standalone: true
 })
 export class HouseGraphic {
+  points: ArrowPoint[] = [
+    { x: 52, y: 25, value: 80, rotation: 0 },
+    { x: 25, y: 42, value: -80, rotation: -90 },
+    { x: 25, y: 77,  value: -80, rotation: -90 },
+    { x: 74, y: 62,  value: -80, rotation: 90 },
+  ];
 
 }
---
+++ solnax-frontend/src/app/model/arrow-point.ts
@@ -0,0 +1,6 @@
+export interface ArrowPoint {
+  x: number;          // Percentage from left (0-100)
+  y: number;          // Percentage from top (0-100)
+  value: number;      // Determines color/direction in the child
+  rotation: number;   // Manual setting
+}
