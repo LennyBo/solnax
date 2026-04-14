db4d4506754da807dc2f4465c12dc078cbe59b29
LennyBo <66992936+LennyBo@users.noreply.github.com>
2026-04-14 08:17:55 +0200

    Update PowerLogManager.java

--- solnax-app/src/main/java/com/rose/solnax/process/PowerLogManager.java
+++ solnax-app/src/main/java/com/rose/solnax/process/PowerLogManager.java
@@ -68,12 +68,12 @@ public class PowerLogManager {
                     PowerLog actual = logMap.get(currentTime);
                     if (actual != null) {
                         // Data exists for this slot
-                        int house = actual.getSolar() + actual.getHouse() - actual.getCharger() - actual.getKitchen();
+                        int house = actual.getSolar() + actual.getHouse() - actual.getCharger() - actual.getKitchen() - actual.getHeater();
                         paddedLogs.getSolar().add(actual.getSolar());
                         paddedLogs.getHouse().add(Math.max(house, 0));
                         paddedLogs.getCharger().add(actual.getCharger());
+                        padderLogs.getHeater().add(actual.getHeater());
                         paddedLogs.getKitchen().add(actual.getKitchen());
-                    }
                 });
 
         return paddedLogs;
