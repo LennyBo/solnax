eb754e8b1b9da0fbfab2652336a4759faf028143
Lenny Boegli <lenny.boegli@post.ch>
2026-04-23 08:52:23 +0200

    Fix test

--- solnax-app/src/test/java/com/rose/solnax/process/ChargeOptimizerTest.java
+++ solnax-app/src/test/java/com/rose/solnax/process/ChargeOptimizerTest.java
@@ -30,7 +30,7 @@ class ChargeOptimizerTest {
     @Test
     void shouldStartChargingWhenExcessPowerAndNotCharging() {
         // house = -4000 (exporting 4000W), charger = 0 → available = 0 - (-4000) - 500 = 3500W
-        when(chargePoint.isCurrentlyCharging()).thenReturn(false);
+
         when(chargePoint.getMinPower()).thenReturn(3450L);
         when(chargePoint.getBatteryLevel()).thenReturn(60);
 
@@ -43,7 +43,7 @@ class ChargeOptimizerTest {
     @Test
     void shouldAdjustAmpsWhenAlreadyCharging() {
         // house = -2000 (exporting 2000W), charger = 3000 → available = 3000 - (-2000) - 500 = 4500W
-        when(chargePoint.isCurrentlyCharging()).thenReturn(true);
+
         when(chargePoint.getMinPower()).thenReturn(3450L);
         when(chargePoint.getBatteryLevel()).thenReturn(60);
 
@@ -57,7 +57,7 @@ class ChargeOptimizerTest {
     @Test
     void shouldStopChargingWhenInsufficientSurplus() {
         // house = 2500 (importing 2500W), charger = 3500 → available = 3500 - 2500 - 500 = 500W (below min)
-        when(chargePoint.isCurrentlyCharging()).thenReturn(true);
+
         when(chargePoint.getMinPower()).thenReturn(3450L);
         when(chargePoint.getBatteryLevel()).thenReturn(60);
 
@@ -70,7 +70,7 @@ class ChargeOptimizerTest {
     @Test
     void shouldNotStartWhenNotEnoughSurplus() {
         // house = -1000 (exporting 1000W), charger = 0 → available = 0 - (-1000) - 500 = 500W (below min)
-        when(chargePoint.isCurrentlyCharging()).thenReturn(false);
+
         when(chargePoint.getMinPower()).thenReturn(3450L);
         when(chargePoint.getBatteryLevel()).thenReturn(60);
 
@@ -84,7 +84,7 @@ class ChargeOptimizerTest {
     @Test
     void shouldNotStopWhenNotCharging() {
         // house = 2500 (importing), charger = 0, not charging → nothing to stop
-        when(chargePoint.isCurrentlyCharging()).thenReturn(false);
+
         when(chargePoint.getMinPower()).thenReturn(3450L);
         when(chargePoint.getBatteryLevel()).thenReturn(60);
 
@@ -98,7 +98,7 @@ class ChargeOptimizerTest {
     @Test
     void shouldDoNothingInNeutralPowerRange() {
         // house = -1000 (exporting 1000W), charger = 0 → available = 500W < 3450W
-        when(chargePoint.isCurrentlyCharging()).thenReturn(false);
+
         when(chargePoint.getMinPower()).thenReturn(3450L);
         when(chargePoint.getBatteryLevel()).thenReturn(60);
 
@@ -111,7 +111,7 @@ class ChargeOptimizerTest {
     @Test
     void shouldStartAtExactMinPowerBoundary() {
         // house = -3950 (exporting 3950W), charger = 0 → available = 3950 - 500 = 3450W = exactly minPower
-        when(chargePoint.isCurrentlyCharging()).thenReturn(false);
+
         when(chargePoint.getMinPower()).thenReturn(3450L);
         when(chargePoint.getBatteryLevel()).thenReturn(60);
 
@@ -125,7 +125,7 @@ class ChargeOptimizerTest {
     void shouldReduceAmpsWhenSurplusDrops() {
         // Charging at 7000W, but surplus dropped: house = 500 (importing 500W)
         // available = 7000 - 500 - 500 = 6000W → adjust down
-        when(chargePoint.isCurrentlyCharging()).thenReturn(true);
+
         when(chargePoint.getMinPower()).thenReturn(3450L);
         when(chargePoint.getBatteryLevel()).thenReturn(60);
 
